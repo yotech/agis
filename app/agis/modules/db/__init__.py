56d3f46e0eca05a566d9e23632b6aa86daa0eecf
Yoel Benítez Fonseca <ybenitezf@gmail.com>
2015-05-19 10:04:02 -0400

    cambios olvidados

---
+++ app/agis/modules/db/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 19/5/2015
+
+@author: Yoel Benítez Fonseca <ybenitezf@gmail.com>
+'''
---
+++ app/agis/modules/db/candiates.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 19/5/2015
+
+@author: Yoel Benítez Fonseca <ybenitezf@gmail.com>
+'''
+from gluon import *
+
+def define_tables():
+    """Poner aquí la definicion de las tablas necesarias para los candidatos"""
+    pass
+
