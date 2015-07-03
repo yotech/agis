2c69c02801d2ad8bacbcfa7eb7e5c456de5e6fdf
Yoel Benítez Fonseca <ybenitezf@gmail.com>
2015-07-03 11:48:17 -0400

    traking sync error

--- app/agis/modules/db/plan_curricular.py
+++ app/agis/modules/db/plan_curricular.py
@@ -21,17 +21,13 @@ def definir_tabla():
     asignatura.definir_tabla()
     if not hasattr( db,'plan_curricular' ):
         db.define_table( 'plan_curricular',
+            Field( 'nombre','string',length=30 ),
             Field( 'carrera_id','reference carrera_uo' ),
-            Field( 'nivel_academico_id','reference nivel_academico' ),
-            Field( 'asignatura_id','reference asignatura' ),
             Field( 'estado','boolean',default=False ),
             )
+        db.plan_curricular.nombre.label=T( 'Nombre del plan' )
+        db.plan_curricular.nombre.requires=[ IS_NOT_EMPTY( error_message=current.T( 'Información requerida' ) ) ]
         db.plan_curricular.carrera_id.label=T( 'Carrera' )
         db.plan_curricular.carrera_id.required=True
-        db.plan_curricular.nivel_academico_id.label=T( 'Nivel académico' )
-        db.plan_curricular.nivel_academico_id.required=True
-        db.plan_curricular.nivel_academico_id.requires = IS_IN_DB( db,'nivel_academico.id','%(nombre)s',zero=None )
-        db.plan_curricular.asignatura_id.label=T( 'Asignatura' )
-        db.plan_curricular.asignatura_id.requires = IS_IN_DB( db,'asignatura.id','%(nombre)s',zero=None )
         db.plan_curricular.estado.label=T( '¿Activo?' )
         db.commit()
