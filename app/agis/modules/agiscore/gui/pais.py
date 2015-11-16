72698d4d9ba6f6ab7bb43e4b96b5ba47854dfec9
Yoel Benítez Fonseca <ybenitezf@gmail.com>
2015-11-16 13:31:36 -0500

    Mejora para #172
    - El país - SIN ESPECIFICAR - no se puede borrar
    - Se pueden hacer busquedas en el grid de los paises

--- app/agis/modules/agiscore/gui/pais.py
+++ app/agis/modules/agiscore/gui/pais.py
@@ -4,9 +4,15 @@ from gluon import *
 from agiscore import tools
 from agiscore.db import pais as model
 
+def puede_borrar(row):
+    if row.codigo == '959':
+        return False
+    return True
+
 def grid_pais():
     db = current.db
     model.definir_tabla()
     db.pais.id.readable = False
     db.pais.id.writable = False
-    return tools.manejo_simple(db.pais, [db.pais.codigo, db.pais.nombre])
+    return tools.manejo_simple(db.pais,
+        [db.pais.codigo, db.pais.nombre], buscar=True, borrar=puede_borrar)
