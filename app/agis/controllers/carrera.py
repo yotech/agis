9de384f221c93239031c22385b73de8baa0495d7
Yoel Benítez Fonseca <ybenitezf@gmail.com>
2015-12-17 12:23:33 -0500

    fix #112, fix #231
    - crea nuevo controlador para carrera
    - arregla gestion de planes y asignaturas

---
+++ app/agis/controllers/carrera.py
@@ -0,0 +1,177 @@
+# -*- coding: utf-8 -*-
+
+if False:
+    from gluon import *
+    from db import *
+    from menu import *
+    from tables import *
+    from gluon.contrib.appconfig import AppConfig
+    from gluon.tools import Auth, Service, PluginManager
+    request = current.request
+    response = current.response
+    session = current.session
+    cache = current.cache
+    T = current.T
+    db = DAL('sqlite://storage.sqlite')
+    myconf = AppConfig(reload=True)
+    auth = Auth(db)
+    service = Service()
+    plugins = PluginManager()
+    from agiscore.gui.mic import MenuLateral, MenuMigas
+    menu_lateral = MenuLateral(list())
+    menu_migas = MenuMigas()
+
+from gluon.storage import Storage
+from agiscore.gui.mic import Accion, grid_simple
+from agiscore.db.carrera_uo import carrera_uo_format
+
+# TODO: remove
+response.menu = []
+
+@auth.requires_login()
+def index():
+    C = Storage()
+    return dict(C=C)
+
+@auth.requires(auth.has_membership(role=myconf.take('roles.admin')))
+def asignaturas():
+    C = Storage()
+    C.plan = db.plan_curricular(int(request.args(0)))
+    C.carrera = db.carrera_uo(C.plan.carrera_id)
+    C.unidad = db.unidad_organica(C.carrera.unidad_organica_id)
+    C.escuela = db.escuela(C.unidad.escuela_id)
+    
+    # breadcumbs
+    # enlace a la UO
+    u_link = Accion(C.unidad.abreviatura or C.unidad.nombre,
+                    URL('unidad', 'index', args=[C.unidad.id]),
+                    True)  # siempre dentro de esta funcion
+    menu_migas.append(u_link)
+    # enlace a la opcion carreras de la UO
+    c_link = Accion(T('Carreras'),
+                    URL('unidad', 'carreras', args=[C.unidad.id]),
+                    True)
+    menu_migas.append(c_link)
+    # planes
+    C.carrera_format = carrera_uo_format(C.carrera)
+    menu_migas.append(C.carrera_format)
+    p_link = Accion(T('Planes'),
+                    URL('planes', args=[C.carrera.id]),
+                    True)
+    menu_migas.append(p_link)
+    menu_migas.append(C.plan.nombre)
+    
+    # permisos
+    puede_crear = auth.has_membership(role=myconf.take('roles.admin'))
+    puede_editar, puede_borrar = (puede_crear, puede_crear)
+    
+    # -- contruir el grid
+    tbl = db.asignatura_plan
+    tbl.id.readable = False
+    query = (tbl.id > 0)
+    query &= (tbl.plan_curricular_id == C.plan.id)
+    
+    tbl.plan_curricular_id.readable = False
+    if 'new' in request.args:
+        tbl.plan_curricular_id.writable = False
+        tbl.plan_curricular_id.default = C.plan.id
+    
+    # validar que no se  repitan las asignaturas por nivel
+    def onvalidation(form):
+        if 'new' in request.args:
+            # comprobar que no exista la combinación nivel
+            n_id = tbl.nivel_academico_id.validate(form.vars.nivel_academico_id)[0]
+            a_id = tbl.asignatura_id.validate(form.vars.asignatura_id)[0]
+            row = tbl(nivel_academico_id=n_id, asignatura_id=a_id)
+            if row:
+                # ya existe en el plan esa asignatura con el mismo nivel de
+                # acceso
+                form.errors.asignatura_id = T("Ya existe en el plan con el mismo nivel")
+    
+    C.grid = grid_simple(query,
+                         args=request.args[:1],
+                         editable=puede_editar,
+                         deletable=puede_borrar,
+                         create=puede_crear,
+                         onvalidation=onvalidation,
+                         orderby=[tbl.nivel_academico_id, tbl.asignatura_id],)
+    
+    return dict(C=C)
+
+@auth.requires(auth.has_membership(role=myconf.take('roles.admin')))
+def planes():
+    '''Manejo de planes para una carrera'''
+    C = Storage()
+    C.carrera = db.carrera_uo(int(request.args(0)))
+    C.unidad = db.unidad_organica(C.carrera.unidad_organica_id)
+    C.escuela = db.escuela(C.unidad.escuela_id)
+    
+    # breadcumbs
+    # enlace a la UO
+    u_link = Accion(C.unidad.abreviatura or C.unidad.nombre,
+                    URL('unidad', 'index', args=[C.unidad.id]),
+                    True)  # siempre dentro de esta funcion
+    menu_migas.append(u_link)
+    # enlace a la opcion carreras de la UO
+    c_link = Accion(T('Carreras'),
+                    URL('unidad', 'carreras', args=[C.unidad.id]),
+                    True)
+    menu_migas.append(c_link)
+    # planes
+    C.carrera_format = carrera_uo_format(C.carrera)
+    menu_migas.append(C.carrera_format)
+    menu_migas.append(T("Planes"))
+    
+    # permisos
+    puede_crear = auth.has_membership(role=myconf.take('roles.admin'))
+    puede_editar, puede_borrar = (puede_crear, puede_crear)
+    
+    # -- contruir el grid de los planes
+    tbl = db.plan_curricular
+    query = ((tbl.id > 0) & (tbl.carrera_id == C.carrera.id))
+    
+    # ver si es la opción de activar plan
+    if 'activar' in request.args:
+        plan = tbl(int(request.args(2)))
+        # desactivar todos los planes
+        db(query).update(estado=False)
+        plan.update_record(estado=True)
+        redirect(URL('carrera', 'planes', args=[C.carrera.id]))
+    
+    tbl.id.readable = False
+    tbl.carrera_id.writable = False
+    
+    if 'new' in request.args:
+        tbl.carrera_id.default = C.carrera.id    
+    
+    campos = [tbl.nombre, tbl.estado]
+    
+    def _enlaces(row):
+        co = CAT()
+        link = URL('asignaturas', args=[row.id])
+        txt = CAT(SPAN('', _class="glyphicon glyphicon-book"),
+                  ' ',
+                  T('Asignaturas'))
+        co.append(Accion(txt, link, True, _class="btn btn-primary btn-sm"))
+        if not row.estado:
+            link = URL('planes',
+                       args=[C.carrera.id, 'activar', row.id],
+                       user_signature=True)
+            txt = CAT(SPAN('', _class="glyphicon glyphicon-ok-sign"),
+                      ' ',
+                      T("Activar"))
+            co.append(Accion(txt, link, True,
+                             _class="btn btn-default btn-sm"))
+        return co
+    
+    enlaces = [dict(header='', body=_enlaces)]
+    
+    C.grid = grid_simple(query,
+                         create=puede_crear,
+                         editable=puede_editar,
+                         deletable=puede_borrar,
+                         fields=campos,
+                         links=enlaces,
+                         args=request.args[:1])
+    
+    return dict(C=C)
--- app/agis/modules/agiscore/gui/carrera_uo.py
+++ app/agis/modules/agiscore/gui/carrera_uo.py
@@ -1,49 +1,49 @@
 # -*- coding: utf-8 -*-
 from gluon import current
 # from gluon.storage import Storage
-from agiscore import tools
-from agiscore.gui.mic import grid_simple
-from agiscore.db import carrera_uo as model
-from gluon.validators import IS_IN_SET
+# from agiscore import tools
+# from agiscore.gui.mic import grid_simple
+# from agiscore.db import carrera_uo as model
+# from gluon.validators import IS_IN_SET
 
 
-def grid_carreras_uo(unidad, db, T,
-                     request=None,
-                     auth=None,
-                     conf=None):
-    request = current.request if request is None else request
-    auth = current.auth if auth is None else auth
-    conf = current.conf if conf is None else conf
-    
-    tbl = db.carrera_uo
-        
-    query = (tbl.id > 0)
-    query &= (tbl.unidad_organica_id == unidad.id)
-    
-    puede_crear = auth.has_membership(role=conf.take('roles.admin'))
-    puede_editar, puede_borrar = (puede_crear, puede_crear)
-    
-    tbl.unidad_organica_id.writable = False
-    tbl.carrera_escuela_id.label = T("Carrera")
-    
-    if 'new' in request.args:
-        tbl.unidad_organica_id.default = unidad.id
-        posibles = model.obtener_posibles(db, unidad.id)
-        tbl.carrera_escuela_id.requires = IS_IN_SET(posibles, zero=None)
-    
-    campos = [tbl.carrera_escuela_id]
-    text_length = {'carrera_uo.carrera_escuela_id': 60}
-    
-    grid = grid_simple(query,
-                       fields=campos,
-                       searchable=False,
-                       editable=puede_editar,
-                       create=puede_crear,
-                       deletable=puede_borrar,
-                       maxtextlengths=text_length,
-                       args=request.args[:1])
-    
-    return grid
+# def grid_carreras_uo(unidad, db, T,
+#                      request=None,
+#                      auth=None,
+#                      conf=None):
+#     request = current.request if request is None else request
+#     auth = current.auth if auth is None else auth
+#     conf = current.conf if conf is None else conf
+#     
+#     tbl = db.carrera_uo
+#         
+#     query = (tbl.id > 0)
+#     query &= (tbl.unidad_organica_id == unidad.id)
+#     
+#     puede_crear = auth.has_membership(role=conf.take('roles.admin'))
+#     puede_editar, puede_borrar = (puede_crear, puede_crear)
+#     
+#     tbl.unidad_organica_id.writable = False
+#     tbl.carrera_escuela_id.label = T("Carrera")
+#     
+#     if 'new' in request.args:
+#         tbl.unidad_organica_id.default = unidad.id
+#         posibles = model.obtener_posibles(db, unidad.id)
+#         tbl.carrera_escuela_id.requires = IS_IN_SET(posibles, zero=None)
+#     
+#     campos = [tbl.carrera_escuela_id]
+#     text_length = {'carrera_uo.carrera_escuela_id': 60}
+#     
+#     grid = grid_simple(query,
+#                        fields=campos,
+#                        searchable=False,
+#                        editable=puede_editar,
+#                        create=puede_crear,
+#                        deletable=puede_borrar,
+#                        maxtextlengths=text_length,
+#                        args=request.args[:1])
+#     
+#     return grid
 
 def seleccionar_carrera(unidad_organica_id=None):
     """Genera un GRID para la selección de una carrera"""
