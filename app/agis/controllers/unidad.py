2a1cd8e39f887d705efa458b2f36476ffda9138f
Yoel Benítez Fonseca <ybenitezf@gmail.com>
2015-12-18 11:02:18 -0500

    cambio

--- app/agis/controllers/unidad.py
+++ app/agis/controllers/unidad.py
@@ -25,12 +25,17 @@ from gluon.storage import Storage
 from agiscore.gui.mic import grid_simple
 from agiscore.gui.mic import Accion
 
+menu_lateral.append(Accion(T('Departamentos'),
+                           URL('departamentos', args=[request.args(0)]),
+                           auth.has_membership(role=myconf.take('roles.admin'))),
+                    ['departamentos'])
 menu_lateral.append(Accion(T('Carreras'),
                            URL('carreras', args=[request.args(0)]),
                            auth.has_membership(role=myconf.take('roles.admin'))),
                     ['carreras'])
 
-#TODO: remove
+
+# TODO: remove
 response.menu = []
 
 @auth.requires_login()
@@ -44,6 +49,26 @@ def index():
     return dict(C=C)
 
 @auth.requires(auth.has_membership(role=myconf.take('roles.admin')))
+def departamentos():
+    C = Storage()
+    C.unidad = db.unidad_organica(int(request.args(0)))
+    C.escuela = db.escuela(C.unidad.escuela_id)
+    
+    # breadcumbs
+    u_link = Accion(C.unidad.abreviatura or C.unidad.nombre,
+                    URL('index', args=[C.unidad.id]),
+                    True)  # siempre dentro de esta funcion
+    menu_migas.append(u_link)
+    menu_migas.append(T('Departamentos'))
+    
+    # -- configurar grid
+    tbl = db.departamento
+    
+    query = (tbl.id > 0) & (tbl.unidad_organica_id == C.unidad.id)
+    
+    return dict(C=C)    
+
+@auth.requires(auth.has_membership(role=myconf.take('roles.admin')))
 def carreras():
     '''configuración de las carreras de la unidad organica'''
     from agiscore.db import carrera_uo as model
@@ -55,7 +80,7 @@ def carreras():
     # breadcumbs
     u_link = Accion(C.unidad.abreviatura or C.unidad.nombre,
                     URL('index', args=[C.unidad.id]),
-                    True) # siempre dentro de esta funcion
+                    True)  # siempre dentro de esta funcion
     menu_migas.append(u_link)
     menu_migas.append(T('Carreras'))
     
