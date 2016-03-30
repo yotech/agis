8c3e9cc5ab3c9eff8feaab8b7b047bbc5e32cdc4
Yoel Benítez Fonseca <ybenitezf@gmail.com>
2016-03-30 12:23:51 -0400

    fix problema con appadmin y el menu

--- app/agis/models/unidad/menu.py
+++ app/agis/models/unidad/menu.py
@@ -1,6 +1,6 @@
 from agiscore.gui.mic import Accion
 
-if not request.ajax:
+if not request.ajax and request.controller != 'appadmin':
     # import si se necesita
     from agiscore.gui.escuela import escuela_menu
     from agiscore.gui.unidad_organica import unidad_menu
