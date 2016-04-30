3c7d85e9a10f28f2e9caa99c2ad197240834a678
Yoel Benítez Fonseca <ybenitezf@gmail.com>
2016-04-30 13:18:33 -0400

    pago de propina arreglado

--- app/agis/modules/agiscore/db/pago_propina.py
+++ app/agis/modules/agiscore/db/pago_propina.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import calendar
+from datetime import date
 from gluon import *
 
 from agiscore.db import persona
@@ -20,6 +21,107 @@ def cantidad_represent(valor, fila):
         return 'N/A'
     return locale.currency(valor, grouping=True)
 
+def pagar_propinas(evento_id, persona_id, db):
+    """Intenta realizar pago de propina para los meses que debe el estudiante"""
+    evento = db.evento(evento_id)
+    persona = db.persona(persona_id)
+    ano = db.ano_academico(evento.ano_academico_id)
+
+    concepto = db(
+        db.tipo_pago.nombre == "PROPINA"
+    ).select().first()
+
+    query  = (db.pago.id > 0)
+    query &= (db.pago.persona_id == persona.id)
+    query &= (db.pago.evento_id == evento.id)
+    query &= (db.pago.tipo_pago_id == concepto.id)
+    sumq = db.pago.cantidad.sum()
+    l_pagos = [p.id for p in db(query).select(db.pago.id)]
+
+    # limpiar las propinas de esta persona
+    db(
+        (db.propina.persona_id == persona.id) &
+        (db.propina.ano_academico_id == ano.id)
+    ).delete()
+    db.commit()
+
+    # meses a pagar
+    meses_a_pagar = ano.meses
+    meses_a_pagar.sort()
+    # print meses_a_pagar
+    consumido = 0.0
+    usados = []
+    multa = 0
+    for mes in meses_a_pagar:
+        # print "Analizando mes: ", mes
+        f_limite = date(int(ano.nombre), mes + 1, ano.dia_limite)
+        # pagos disponibles
+        query  = (db.pago.id > 0)
+        query &= (db.pago.persona_id == persona.id)
+        query &= (db.pago.evento_id == evento.id)
+        query &= (db.pago.tipo_pago_id == concepto.id)
+        query &= (db.pago.fecha_recivo < f_limite)
+
+        # saldo disponible para el mes
+        saldo_mes = db(query).select(sumq).first()[sumq] or 0.0
+        saldo_mes = saldo_mes - consumido
+        if saldo_mes >= concepto.cantidad:
+            multa = 0
+            l_pagos = [p.id for p in db(query).select(db.pago.id)]
+            l_pagos.sort()
+            # se puede hacer el pago de propina con estos pagos
+            db.propina.insert(
+                pago_id=l_pagos,
+                mes=mes,
+                cantidad=concepto.cantidad,
+                multa=multa,
+                persona_id=persona.id,
+                ano_academico_id=ano.id
+            )
+            usados.extend(l_pagos)
+            consumido += concepto.cantidad
+        else:
+            # hay que tener encuenta todos los pagos y aplicar la multa.
+            query  = (db.pago.id > 0)
+            query &= (db.pago.persona_id == persona.id)
+            query &= (db.pago.evento_id == evento.id)
+            query &= (db.pago.tipo_pago_id == concepto.id)
+            saldo_mes = db(query).select(sumq).first()[sumq] or 0.0
+            saldo_mes = saldo_mes - consumido
+            multa = (concepto.cantidad * ano.multa) / 100
+            if saldo_mes >= (concepto.cantidad + multa):
+                # se hace el pago con multa
+                l_pagos = [p.id for p in db(query).select(db.pago.id)]
+                l_pagos.sort()
+                db.propina.insert(
+                    pago_id=l_pagos,
+                    mes=mes,
+                    cantidad=concepto.cantidad + multa,
+                    multa=multa,
+                    persona_id=persona.id,
+                    ano_academico_id=ano.id
+                )
+                usados.extend(l_pagos)
+                consumido += (concepto.cantidad + multa)
+            else:
+                break
+    # if multa > 0:
+        # si multa > 0 entonces es que hay deudas
+        # pros = db(
+        #     (db.propina.persona_id == persona.id) &
+        #     (db.propina.ano_academico_id == ano.id)
+        # ).select()
+        # meses_pagos = [p.mes for p in pros]
+        # meses_pagos.sort()
+        # a_pagar = list(set(ano.meses) - set(meses_pagos))
+        # a_pagar.sort() # a_pagar[1] es el proximo mes cuando se aplica multa
+        # f_limite = date(int(ano.nombre), date.today().month() + 1, ano.dia_limite)
+        # from agiscore.db.matricula import MATRICULADO_CON_DEUDAS
+        # est = db.estudiante(persona_id=persona.id)
+        # mat = db.matricula(estudiante_id=est.id, ano_academico_id=ano.id)
+        # mat.update_record(estado_uo=MATRICULADO_CON_DEUDAS)
+    db.commit()
+
 def definir_tabla(db=None, T=None):
     if db is None:
         db = current.db
@@ -29,11 +131,15 @@ def definir_tabla(db=None, T=None):
     pago.definir_tabla(db, T)
     if not hasattr(db, 'propina'):
         tbl = db.define_table('propina',
-            Field('pago_id', 'reference pago'),
+            Field('pago_id', 'list:reference pago'),
             Field('mes', 'integer'),
             Field('cantidad', 'float'),
             Field('multa', 'float'),
+            Field('persona_id', 'reference persona'),
+            Field('ano_academico_id', 'reference ano_academico')
         )
+        tbl.persona_id.readable = False
+        tbl.ano_academico_id.readable = False
         tbl.mes.label = T("Mes")
         tbl.pago_id.label = T("Info. Pago")
         tbl.cantidad.label = T("Cantidad")
